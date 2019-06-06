8b4a4d5633701745bb5a2bc16145c270b5244911
Jeremy Erickson <jeremyerickson31@gmail.com>
2019-06-06 13:33:04 -0400

    add graph labels

--- correlated_randoms/correlated_randoms_with_cholesky.py
+++ correlated_randoms/correlated_randoms_with_cholesky.py
@@ -10,7 +10,7 @@ mean = 0.0
 std_dev = 1.0
 
 correlation_matrix_low = numpy.array([[1.0, 0.3], [0.3, 1.0]])
-correlation_matrix_high = numpy.array([[1.0, 0.9], [0.9, 1.0]])
+correlation_matrix_high = numpy.array([[1.0, 0.95], [0.95, 1.0]])
 rands = numpy.random.normal(mean, std_dev, size=(vars, rand_draws))
 upper_cholesky_low_corr = cholesky(correlation_matrix_low)
 upper_cholesky_high_corr = cholesky(correlation_matrix_high)
@@ -24,12 +24,19 @@ correlated_rands_corr_coeff_high, p_high = pearsonr(correlated_rands_high_corr[0
                                                     correlated_rands_high_corr[1, :])
 
 pyplot.subplot(2, 2, 1)
-pyplot.scatter(rands[0, :], rands[1, :])
+pyplot.scatter(rands[0, :], rands[1, :], s=1, marker="o")
+pyplot.xlabel("Corr=0, actual=" + str(round(rands_corr_coeff, 4)))
+
 pyplot.subplot(2, 2, 2)
-pyplot.scatter(correlated_rands_low_corr[0, :], correlated_rands_low_corr[1, :])
+pyplot.scatter(correlated_rands_low_corr[0, :], correlated_rands_low_corr[1, :],s=1, marker="o")
+pyplot.xlabel("Corr=0.3, actual=" + str(round(correlated_rands_corr_coeff_low, 4)))
+
 pyplot.subplot(2, 2, 3)
-pyplot.scatter(rands[0, :], rands[1, :])
+pyplot.scatter(rands[0, :], rands[1, :], s=1, marker="o")
+pyplot.xlabel("Corr=0, actual=" + str(round(rands_corr_coeff, 4)))
+
 pyplot.subplot(2, 2, 4)
-pyplot.scatter(correlated_rands_high_corr[0, :], correlated_rands_high_corr[1, :])
+pyplot.scatter(correlated_rands_high_corr[0, :], correlated_rands_high_corr[1, :], s=1, marker="o")
+pyplot.xlabel("Corr=0.95, actual=" + str(round(correlated_rands_corr_coeff_high, 4)))
 
 pyplot.show()
